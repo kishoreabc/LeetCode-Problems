1e7325840519cbc58a33dcdec96af99662761837
Kishore R <90026767+kishoreabc@users.noreply.github.com>
2025-02-05 21:16:47 +0530

    Update 168. Excel Sheet Column Title.py

--- 168. Excel Sheet Column Title.py
+++ 168. Excel Sheet Column Title.py
@@ -2,7 +2,7 @@ class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         result = ""
         while columnNumber>26:
-            result+=chr((columnNumber-1)%26 +65)
+            result = chr((columnNumber-1)%26 + ord('A')) + result
             columnNumber = (columnNumber-1) // 26
-        result += chr((columnNumber-1)+65) if columnNumber else ""
-        return result[::-1]
+        result = (chr((columnNumber-1)+ ord('A')) if columnNumber else "") + result
+        return result
